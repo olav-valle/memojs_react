9a7e609482f974b8d9d0015370a3000e30d947c5
Olav Valle <olavval@stud.ntnu.no>
2021-03-26 12:27:04 +0100

    repo restructuring to accommodate backend

--- src/components/CardButton.js
+++
@@ -1,9 +0,0 @@
-
-function CardButton(props) {
-    //todo: Should button components be rendered using some form of {props.children}?
-    return (
-        <button className={`cardButton ${props.className}`} onClick={props.onClick}></button>
-        )
-}
-
-export default CardButton
